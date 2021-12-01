22b9ec0ab32973712905ae8b3e58785a123696aa
cjdrakeUSCUP <60363127+cjdrakeUSCUP@users.noreply.github.com>
2021-12-01 16:51:17 -0500

    Delete jarFiles directory

--- jarFiles/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module CSCI540 {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires javafx.base;
-	requires java.sql;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
